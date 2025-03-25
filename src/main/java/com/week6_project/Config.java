f56f9c2ebeaab5beb2ef128c325d2d9745ea81a0
Julius Agbame <julius.agbame@amalitech.com>
2025-03-25 14:42:38 +0000

    Change the region

--- src/main/java/com/week6_project/Config.java
+++ src/main/java/com/week6_project/Config.java
@@ -1,7 +1,6 @@
 package com.week6_project;
 
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
@@ -11,7 +10,6 @@ import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
 import com.amazonaws.services.secretsmanager.model.GetSecretValueResult;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +17,8 @@ import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 
-import static software.amazon.awssdk.regions.Region.US_WEST_2;
+import static software.amazon.awssdk.regions.Region.US_EAST_2;
+
 
 @Configuration
 public class Config {
@@ -30,7 +29,7 @@ public class Config {
         try {
             AWSSecretsManager client = AWSSecretsManagerClient.builder()
                     .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
-                    .withRegion(String.valueOf(US_WEST_2))
+                    .withRegion(String.valueOf(US_EAST_2))
                     .build();
 
             GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest();
@@ -63,11 +62,11 @@ public class Config {
     }
 
     @Bean
-    public AmazonS3 s3client(){
+    public AmazonS3 s3Client(){
         return AmazonS3Client
                 .builder()
                 .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
-                .withRegion(Regions.EU_CENTRAL_1)
+                .withRegion(Regions.US_EAST_2)
                 .build();
     }
 }
